92b171d0060dc7cd3cb53643a32b6cdacd2875ea
Prime Patel <primespatel@gmail.com>
2023-02-23 21:37:26 +0530

    added sensors

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import pygame
+import pygame, sys
 from math import sin, cos, atan, sqrt
 from math import radians as rad
-
+ROAD_COLOR = (58, 58, 60, 255)
 pygame.init()
 
 class Car:
@@ -13,10 +13,17 @@ class Car:
         self.angle = 0
         self.position = [315, 455]
         self.edges = [[self.position[0]-15, self.position[1]-10], [self.position[0]+15, self.position[1]-10], [self.position[0]+15, self.position[1]+10], [self.position[0]-15, self.position[1]+10]]
+        self.sensor_data = []
 
     def draw(self):
         self.rotated_car = pygame.transform.rotate(self.car, -self.angle)
         screen.blit(self.rotated_car, (int(self.position[0])-15, int(self.position[1])-10))
+        self.sensor_data.clear()
+        loc = [int(abs((self.edges[1][0]+self.edges[2][0])/2)), int(abs((self.edges[1][1]+self.edges[2][1])/2))]
+        for i in self.get_sensor_data():
+            boundary = i[0]
+            pygame.draw.line(screen, (0, 255, 0),loc , boundary, 1)
+            pygame.draw.circle(screen, (0, 255, 0), boundary, 5)
 
     def set_edges(self, cords):
         r, theta = sqrt(15**2 + 10**2), atan(2/3)
@@ -29,11 +36,24 @@ class Car:
     def check_crash(self):
         for i in self.edges:
             # print(game_map.get_at(tuple(i)))
-            if game_map.get_at(tuple(i)) != (58, 58, 60, 255):
+            if game_map.get_at(tuple(i)) != ROAD_COLOR:
                 print("crashed")
                 return True
         print("not crashed")
         return False
+    
+    def get_sensor_data(self):
+        for sensor_angle in range(-90, 91, 30):
+            x, y = int(abs((self.edges[1][0]+self.edges[2][0])/2)), int(abs((self.edges[1][1]+self.edges[2][1])/2))
+            length = 0
+            a, b = x, y
+            while game_map.get_at((x, y)) == ROAD_COLOR and length < 150:
+                length = length + 1
+                x = int(a + cos(rad( self.angle + sensor_angle)) * length)
+                y = int(b + sin(rad( self.angle + sensor_angle)) * length)
+            dist = int((pow(x - a, 2) + pow(y - b, 2))**0.5)
+            self.sensor_data.append([(x, y), dist])
+        return self.sensor_data
 
 screen = pygame.display.set_mode((960, 540))
 game_map = pygame.image.load('.\\maps\\PNG\\map.png').convert()
