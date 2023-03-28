e8d40fa7b742ff092d1c2e884acc9cea50fdd33b
Prime Patel <primespatel@gmail.com>
2023-03-28 19:35:46 +0530

    added game class

--- main.py
+++ main.py
@@ -1,9 +1,52 @@
-import pygame, sys
+import pygame
 from math import sin, cos, atan, sqrt
 from math import radians as rad
 ROAD_COLOR = (58, 58, 60, 255)
 pygame.init()
+screen = pygame.display.set_mode((960, 540))
 
+class Game:
+    def __init__(self) -> None:
+        self.game_map = pygame.image.load('.\\maps\\PNG\\map.png').convert()
+        self.game_map = pygame.transform.scale(self.game_map, (960, 540))
+        
+    def run(self):
+        car = Car()
+        game_run = True
+        while game_run:
+            screen.blit(self.game_map, (0, 0))
+            key = pygame.key.get_pressed()
+            if key[pygame.K_UP] == True:
+                cords = (int(car.position[0] + cos(rad(car.angle))), int(car.position[1] + sin(rad(car.angle))))
+                car.set_edges(cords)
+                car.check_crash(self.game_map)
+                car.position[0], car.position[1] = car.position[0] + cos(rad(car.angle)), car.position[1] + sin(rad(car.angle))
+            elif key[pygame.K_DOWN] == True:
+                cords = (int(car.position[0] + cos(rad(car.angle))), int(car.position[1] + sin(rad(car.angle))))
+                car.set_edges(cords)
+                car.check_crash(self.game_map)
+                car.position[0], car.position[1] = car.position[0] - cos(rad(car.angle)), car.position[1] - sin(rad(car.angle))
+            
+            elif key[pygame.K_RIGHT] == True:
+                cords = (int(car.position[0] + cos(rad(car.angle))), int(car.position[1] + sin(rad(car.angle))))
+                car.set_edges(cords)
+                car.check_crash(self.game_map)
+                car.angle += 1
+            
+            elif key[pygame.K_LEFT] == True:
+                cords = (int(car.position[0] + cos(rad(car.angle))), int(car.position[1] + sin(rad(car.angle))))
+                car.set_edges(cords)
+                car.check_crash(self.game_map)
+                car.angle -= 1
+
+            car.draw(self.game_map)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game_run = False
+                    break
+            pygame.display.update()
+        pygame.quit()
+        
 class Car:
     def __init__(self):
         self.car = pygame.image.load('car.png').convert()
@@ -15,11 +58,11 @@ class Car:
         self.edges = [[self.position[0]-15, self.position[1]-10], [self.position[0]+15, self.position[1]-10], [self.position[0]+15, self.position[1]+10], [self.position[0]-15, self.position[1]+10]]
         self.sensor_data = []
 
-    def draw(self):
+    def draw(self, map):
         self.rotated_car = pygame.transform.rotate(self.car, -self.angle)
         screen.blit(self.rotated_car, (int(self.position[0])-15, int(self.position[1])-10))
         self.sensor_data.clear()
-        for i in self.get_sensor_data():
+        for i in self.get_sensor_data(map):
             boundary = i[0]
             pygame.draw.line(screen, (0, 255, 0), self.position, boundary, 1)
             pygame.draw.circle(screen, (0, 255, 0), boundary, 5)
@@ -32,20 +75,20 @@ class Car:
         self.edges[2][0], self.edges[2][1] = cords[0] + dx, cords[1] + dy
         self.edges[3][0], self.edges[3][1] = cords[0] - dx, cords[1] + dy
 
-    def check_crash(self):
+    def check_crash(self, map):
         for i in self.edges:
-            if game_map.get_at(tuple(i)) != ROAD_COLOR:
+            if map.get_at(tuple(i)) != ROAD_COLOR:
                 print("crashed")
                 return True
         print("not crashed")
         return False
     
-    def get_sensor_data(self):
+    def get_sensor_data(self, map):
         for sensor_angle in range(-90, 91, 30):
             x, y = int(self.position[0]), int(self.position[1])
             length = 0
             a, b = x, y
-            while game_map.get_at((x, y)) == ROAD_COLOR and length < 150:
+            while map.get_at((x, y)) == ROAD_COLOR and length < 150:
                 length = length + 1
                 x = int(a + cos(rad( self.angle + sensor_angle)) * length)
                 y = int(b + sin(rad( self.angle + sensor_angle)) * length)
@@ -53,43 +96,6 @@ class Car:
             self.sensor_data.append([(x, y), dist])
         return self.sensor_data
 
-screen = pygame.display.set_mode((960, 540))
-game_map = pygame.image.load('.\\maps\\PNG\\map.png').convert()
-game_map = pygame.transform.scale(game_map, (960, 540))
-car = Car()
-
-run = True
-while run:
-    screen.blit(game_map, (0, 0))
-    key = pygame.key.get_pressed()
-    if key[pygame.K_UP] == True:
-        cords = (int(car.position[0] + cos(rad(car.angle))), int(car.position[1] + sin(rad(car.angle))))
-        car.set_edges(cords)
-        car.check_crash()
-        car.position[0], car.position[1] = car.position[0] + cos(rad(car.angle)), car.position[1] + sin(rad(car.angle))
-    elif key[pygame.K_DOWN] == True:
-        cords = (int(car.position[0] + cos(rad(car.angle))), int(car.position[1] + sin(rad(car.angle))))
-        car.set_edges(cords)
-        car.check_crash()
-        car.position[0], car.position[1] = car.position[0] - cos(rad(car.angle)), car.position[1] - sin(rad(car.angle))
-    
-    elif key[pygame.K_RIGHT] == True:
-        cords = (int(car.position[0] + cos(rad(car.angle))), int(car.position[1] + sin(rad(car.angle))))
-        car.set_edges(cords)
-        car.check_crash()
-        car.angle += 1
-    
-    elif key[pygame.K_LEFT] == True:
-        cords = (int(car.position[0] + cos(rad(car.angle))), int(car.position[1] + sin(rad(car.angle))))
-        car.set_edges(cords)
-        car.check_crash()
-        car.angle -= 1
-
-    car.draw()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            break
-    pygame.display.update()
-
-pygame.quit()
+if __name__ == "__main__":
+    game = Game()
+    game.run()
